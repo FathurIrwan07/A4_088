b30b3b53c100f8a8cf2f1db3acaf3174e3e743a0
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2025-01-27 15:48:50 +0700

    tambah interface ManajerPropertiService untuk operasi CRUD ManajerProperti

---
+++ app/src/main/java/com/dev/properti/service/ManajerPropertiService.kt
@@ -0,0 +1,39 @@
+package com.dev.properti.service
+
+import com.dev.properti.model.ManajerProperti
+import com.dev.properti.model.ManajerPropertiResponse
+import com.dev.properti.model.ManajerPropertiResponseDetail
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ManajerPropertiService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+
+    @GET("manajerproperti")
+    suspend fun getAllManajer(): ManajerPropertiResponse
+
+    @POST("manajerproperti/store")
+    suspend fun insertManajer(@Body manajerProperti: ManajerProperti)
+
+
+    @GET("manajerproperti/{id_manajer}")
+    suspend fun getManajerById(@Path("{id_manajer}") id: Int): ManajerPropertiResponseDetail
+
+    @PUT("manajerproperti/{id_manajer}")
+    suspend fun updateManajer(@Path("{id_manajer}") id: Int, @Body manajerProperti: ManajerProperti)
+
+    @DELETE("{manajerproperti/{id}")
+    suspend fun deleteManajer(@Path("{id_manajer}") id: Int): Response<Void>
+
+}
